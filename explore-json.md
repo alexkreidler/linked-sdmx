8e5d0b7f8313da889664759a86164615f160d3e3
Alex Kreidler <alexkreidler2020@gmail.com>
2021-03-30 15:44:21 -0400

    commit most recent changes

---
+++ explore-json.md
@@ -0,0 +1,3 @@
+Using quicktype to analyze the structure of Dasel XML to JSON files.
+
+So far the TS definitions are not sorted, so thats what the diff mainly focuses on.
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,6 @@ structure="../data/WB.KeyFamily.xml"
 output="${input%.xml}.rdf"
 
 # -t -tree:linked -ext:on
-xslt -l -s:$input -xsl:generic.xsl -o:$output xmlDocument=$input pathToGenericStructure=$structure agencyURI=https://data.loqu.dev/WB/
+# xslt -l -s:$input -xsl:generic.xsl -o:$output xmlDocument=$input pathToGenericStructure=$structure agencyURI=https://data.loqu.dev/WB/
+
+xslt -t -tree:linked -ext:on -s:../data/WB.KeyFamily.xml -xsl:generic.xsl xmlDocument=../data/WB.KeyFamily.xml pathToGenericStructure=../data/WB.KeyFamily.xml >../data/WB.KeyFamily.rdf
